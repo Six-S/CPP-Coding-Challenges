b291aa200738c4e1c7ba56f8e14db2c813e2aad6
Six-S <brenlink@gmail.com>
2019-12-01 20:26:18 -0700

    Structure, put already started file into push

---
+++ RedditChallenges/challenge1Reddit.cpp
@@ -0,0 +1,13 @@
+//morse code generator challenge. By: Six-S
+//This question was asked by u/Cosmologicon
+//https://www.reddit.com/r/dailyprogrammer/comments/cmd1hb/20190805_challenge_380_easy_smooshed_morse_code_1/
+
+#include<iostream>
+#include<string>
+
+std::string morse = ".- -... -.-. -.. . ..-. --. .... .. .--- -.- .-.. -- -. --- .--. --.- .-. ... - ..- ...- .-- -..- -.-- --..";
+
+int main()
+{
+
+}
